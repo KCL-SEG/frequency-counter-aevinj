ac703572d24fd505a440f2825d9c3686c628d2a0
Aevin Jais <aevin.jais@kcl.ac.uk>
2022-10-11 23:37:10 +0100

    Add frequencies functionality.

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    single_list = []
+
+    for i in range(len(items)):
+        if type(items[i]) != "str":
+            items[i] = str(items[i])
+
+    for i in range(len(items)):
+        if not(items[i] in single_list):
+            single_list.append(items[i])
+            frequencies[str(items[i])] = 1
+        else:
+            frequencies[str(items[i])] +=1
     return frequencies
